2f93237d75140acdfa42041361004aae04552f3d
hung0110 <ngovanhung01102002@gmail.com>
2022-06-10 10:08:40 +0700

    up date

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,10 @@ module.exports = defineConfig({
         main: resolve(__dirname, 'index.html'),
         detail: resolve(__dirname, 'detail.html'),
         products: resolve(__dirname, 'products.html'),
-        asm: resolve (__dirname, 'asm.html')
+        asm: resolve(__dirname, 'asm.html'),
+        asm2: resolve(__dirname, 'asm2.html'),
+        asm3: resolve(__dirname, 'asm3.html'),
+        asm4: resolve(__dirname, 'asm3.html'),
       }
     }
   }
